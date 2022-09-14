99736b9638f86b06eae665f7b84ac1159d80b12a
ngusu <musyimi@users.noreply.github.com>
2022-09-14 11:23:16 +0300

    updating structs

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -6,10 +6,9 @@ defmodule Identicon do
     |> pick_color
   end
 
-  def pick_color(image) do
-    %Identicon.Image{hex: [r,g,b | _tail]} = image
+  def pick_color(%Identicon.Image{hex: [r,g,b | _tail]} = image) do
 
-    [r,g,b]
+    %Identicon.Image{image | color: {r,g,b}}
   end
   
   def hash_input(input) do
